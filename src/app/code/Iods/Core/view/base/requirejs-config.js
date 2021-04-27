b63fc83f0c9f5949e6358d2e2e730fa72f16056c
Rye Miller <millerrye17@gmail.com>
2021-04-26 23:00:00 -0600

    Addition of core views for sample Magento projects.

---
+++ src/app/code/Iods/Core/view/base/requirejs-config.js
@@ -0,0 +1,1 @@
+const config={map:{"*":{DholiMask:"Dholi_Core/js/masks"}}};
---
+++ src/app/code/Iods/Core/view/frontend/requirejs-config.js
@@ -0,0 +1,21 @@
+/**
+ * Core module for extending and testing functionality across Magento 2
+ *
+ * @package   Iods_Core
+ * @author    Rye Miller <rye@drkstr.dev>
+ * @copyright Copyright (c) 2021, Rye Miller (https://ryemiller.io)
+ * @license   See LICENSE for license details.
+ */
+
+var config = {
+    paths: {
+        'mageplaza/core/jquery/popup': 'Mageplaza_Core/js/jquery.magnific-popup.min',
+        'mageplaza/core/owl.carousel': 'Mageplaza_Core/js/owl.carousel.min',
+        'mageplaza/core/bootstrap': 'Mageplaza_Core/js/bootstrap.min'
+    },
+    shim: {
+        "mageplaza/core/jquery/popup": ["jquery"],
+        "mageplaza/core/owl.carousel": ["jquery"],
+        "mageplaza/core/bootstrap": ["jquery"]
+    }
+};
