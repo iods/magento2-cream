ae43de0e452875f38f9f162af7334ac45779f707
Rye Miller <millerrye17@gmail.com>
2021-04-26 22:27:27 -0600

    Addition of core testing files for sample Magento projects.

---
+++ src/app/code/Iods/Core/Test/Unit/Service/Formatter/AddressFormatterTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * It is available through the world-wide-web at this URL:
+ * https://tldrlegal.com/license/mit-license
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to support@buckaroo.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact support@buckaroo.nl for more information.
+ *
+ * @copyright Copyright (c) Buckaroo B.V.
+ * @license   https://tldrlegal.com/license/mit-license
+ */
+namespace Buckaroo\Magento2\Test\Unit\Service\Formatter;
+
+use Buckaroo\Magento2\Service\Formatter\AddressFormatter;
+use Buckaroo\Magento2\Test\BaseTest;
+
+class AddressFormatterTest extends BaseTest
+{
+    protected $instanceClass = AddressFormatter::class;
+
+    public function testFormat()
+    {
+        $streetFormatter = $this->getObject(\Buckaroo\Magento2\Service\Formatter\Address\StreetFormatter::class);
+        $phoneFormatter = $this->getObject(\Buckaroo\Magento2\Service\Formatter\Address\PhoneFormatter::class);
+
+        $addressMock = $this->getFakeMock(\Magento\Sales\Api\Data\OrderAddressInterface::class)->getMock();
+        $addressMock->method('getCountryId')->willReturn('NL');
+
+        $instance = $this->getInstance(['streetFormatter' => $streetFormatter, 'phoneFormatter' => $phoneFormatter]);
+        $result = $instance->format($addressMock);
+
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('street', $result);
+        $this->assertArrayHasKey('telephone', $result);
+        $this->assertCount(3, $result['street']);
+        $this->assertCount(4, $result['telephone']);
+    }
+}
