3a6b0cef6a4e00017420cdaab802f5d3266a5b11
Rye Miller <millerrye17@gmail.com>
2021-04-26 22:22:46 -0600

    Addition of core fixes for sample Magento projects.

---
+++ src/app/code/Iods/Core/MessageQueue/DummyConnectionTypeResolver.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Core module for extending and testing functionality across Magento 2
+ *
+ * @package   Iods_Core
+ * @author    Rye Miller <rye@drkstr.dev>
+ * @copyright Copyright (c) 2021, Rye Miller (https://ryemiller.io)
+ * @license   See LICENSE for license details.
+ */
+declare(strict_types=1);
+
+namespace Iods\Core\MessageQueue;
+
+use Magento\Framework\MessageQueue\ConnectionTypeResolverInterface;
+
+class DummyConnectionTypeResolver implements ConnectionTypeResolverInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getConnectionType($connectionName)
+    {
+        return 'foobar';
+    }
+}
