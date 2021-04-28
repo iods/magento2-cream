5475c3324c102cbf66732518725d2064a6f27733
Rye Miller <millerrye17@gmail.com>
2021-04-27 21:14:43 -0600

    Update of module configs for sample Magento projects.

--- src/app/code/Iods/Api/registration.php
+++ src/app/code/Iods/Api/registration.php
@@ -1,7 +1,16 @@
 <?php
+/**
+ * @package   Iods_Api
+ * @author    Rye Miller <rye@drkstr.dev>
+ * @copyright Copyright © 2020, Rye Miller (https://ryemiller.io)
+ * @license   See LICENSE for license details.
+ */
+declare(strict_types=1);
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AHT_ModuleHelloWorld',
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Iods_Api',
     __DIR__
 );
